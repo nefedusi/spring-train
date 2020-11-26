8e91709fadeb388fc39092af636178d24436fa41
Dmitry Nefedov <nefedusi@gmail.com>
2020-11-26 15:27:29 +0700

    Добавлена авторизация с использованием аннотаций

--- src/main/java/org/example/springtrain/controller/SecurityCheckController.java
+++ src/main/java/org/example/springtrain/controller/SecurityCheckController.java
@@ -1,7 +1,12 @@
 package org.example.springtrain.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class SecurityCheckController {
@@ -43,5 +48,57 @@ public class SecurityCheckController {
     @GetMapping("/safe/only-admin")
     public String adminAllowed() {
         return "Endpoint allowed only for admin";
+    }
+
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    @GetMapping("/safe/secured/user-and-admin")
+    public String securedUserAndAdminAllowed() {
+        return "Endpoint allowed for user and admin roles. Secured annotation";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/safe/secured/only-admin")
+    public String securedAdminAllowed() {
+        return "Endpoint allowed only for admin. Secured annotation";
+    }
+
+    @RolesAllowed({"ROLE_USER", "ROLE_ADMIN"})
+    @GetMapping("/safe/roles-allowed/user-and-admin")
+    public String rolesAllowedUserAndAdminAllowed() {
+        return "Endpoint allowed for user and admin roles. RolesAllowed annotation";
+    }
+
+    @RolesAllowed("ROLE_ADMIN")
+    @GetMapping("/safe/roles-allowed/only-admin")
+    public String rolesAllowedAdminAllowed() {
+        return "Endpoint allowed only for admin. RolesAllowed annotation";
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/safe/pre-authorize/user-and-admin")
+    public String preAuthorizeUserAndAdminAllowed() {
+        return "Endpoint allowed for user and admin roles. PreAuthorize annotation";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/safe/pre-authorize/only-admin")
+    public String preAuthorizeAdminAllowed() {
+        return "Endpoint allowed only for admin. PreAuthorize annotation";
+    }
+
+    @PostAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/safe/post-authorize/user-and-admin")
+    public String postAuthorizeUserAndAdminAllowed() {
+        System.out.println("postAuthorizeUserAndAdminAllowed method called");
+        return "Endpoint allowed for user and admin roles. PostAuthorize annotation";
+    }
+
+    //Если вызвать метод от лица user с НЕправильным паролем, то метод не вызовется.
+    //А если вызвать от лица user с правильным - то вызовется, но результат не вернётся клиенту.
+    @PostAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/safe/post-authorize/only-admin")
+    public String postAuthorizeAdminAllowed() {
+        System.out.println("postAuthorizeUserAndAdminAllowed method called");
+        return "Endpoint allowed only for admin. PostAuthorize annotation";
     }
 }
